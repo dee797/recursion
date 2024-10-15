8fe7c4da5fbf9c288d37a44dd9514ed3670518a0
dee <178827694+dee797@users.noreply.github.com>
2024-10-15 13:32:13 -0600

    Create merge sort recursive function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const fibonacci = function(input) {
     const arr = [0, 1];
 
-    input = parseInt(input);
-    if (input < 0 || !Number.isInteger(input)) return "OOPS";
+    const int = parseInt(input);
+    if (int < 0 || !Number.isInteger(int)) return "OOPS";
 
     arr[input] = input;
     [...arr].map((currentValue, index) => {
@@ -16,7 +16,7 @@ const fibonacci = function(input) {
 };
 
 
-// console.log(fibonacci(8));
+console.log(fibonacci(8));
 
 
 const fibsRec = function(n, i=2, array=[0,1]) {
@@ -34,6 +34,45 @@ const fibsRec = function(n, i=2, array=[0,1]) {
 }
 
 
-// console.log(fibsRec(15));
+console.log(fibsRec(15));
 
 
+const mergeSort = function(array) {
+
+    const splitIndex = Math.floor(array.length / 2)
+    let leftHalf = array.slice(0, splitIndex);
+    let rightHalf = array.slice(splitIndex);
+    const newArray = [];
+
+    if (leftHalf.length > 1) leftHalf = mergeSort(leftHalf);
+    if (rightHalf.length > 1) rightHalf = mergeSort(rightHalf);
+
+    const copyLeft = leftHalf.slice();
+    const copyRight = rightHalf.slice();
+    let i = 0;
+    let j = 0;
+
+    while (i < leftHalf.length && j < rightHalf.length) {
+        if (leftHalf[i] < rightHalf[j]) {
+            newArray.push(leftHalf[i]);
+            copyLeft.shift();
+            i++
+        } else {
+            newArray.push(rightHalf[j]);
+            copyRight.shift();
+            j++
+        }
+    }
+
+    if (copyLeft.length === 0) {
+        for (const item of copyRight) newArray.push(item);
+    }
+    else if (copyRight.length === 0) {
+        for (const item of copyLeft) newArray.push(item);
+    }
+
+    return newArray;
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
